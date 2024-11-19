486043225114ff6f4a9db8d36c5fbe9c2fb53de6
Thomas W. <96929397+ctwyrth@users.noreply.github.com>
2024-11-18 19:40:20 -0800

    finished strip and sort, added report

--- main.py
+++ main.py
@@ -8,12 +8,26 @@
 # if __name__ == '__main__':
 #   sys.exit(main())
 import json
+import re
 
 def main():
   path_to = "books/frankenstein.txt"
   book_text = get_text(path_to)
   num_words_in_book = count_words(book_text)
   char_count_in_book = count_chars(book_text)
+
+  sorted_char_count = {k: v for k, v in sorted(char_count_in_book.items(), key = lambda item: item[1], reverse = True)}
+
+  print("--- Begin report of books/frankenstein.txt ---")
+  print(f"{num_words_in_book} words found in document")
+  print()
+  
+  # print(json.dumps(sorted_char_count))
+
+  for key in sorted_char_count:
+    print(f"The '{key}' character was found {sorted_char_count[key]} times")
+  
+  print("--- End report ---")
 
 
 def get_text(path):
@@ -28,7 +42,10 @@ def count_words(text):
 def count_chars(text):
   char_dict = {}
 
+  text = re.sub('[^a-zA-Z]', '', text)
   text = text.lower()
+
+  # print(text)
 
   for char in text:
     if char_dict.get(char):
