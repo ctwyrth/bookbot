0409c00842bd743959ea9aa545078db14a652b6c
Thomas W. <96929397+ctwyrth@users.noreply.github.com>
2025-07-06 11:55:21 -0700

    dynamic variable

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-# import sys
+import sys
 from stats import count_words, count_chars, sort_chars
-
-# def main() -> str:
-#   with open('books/frankenstein.txt') as f:
-#     file_contents = f.read()
-#     print(file_contents)
-
-# if __name__ == '__main__':
-#   sys.exit(main())
 import json
-# import re
 
 def main():
-  path_to = "books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit()
+  # path_to = "books/frankenstein.txt"
+  path_to = sys.argv[1]
   book_text = get_text(path_to)
   num_words_in_book = count_words(book_text)
   char_count_in_book = count_chars(book_text)
   sorted_chars_in_book = sort_chars(char_count_in_book)
 
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {path_to}...")
   print("----------- Word Count ----------")
   # print(f"{num_words_in_book} words found in the document")
   print(f"Found {num_words_in_book} total words")
@@ -41,25 +36,6 @@ def get_text(path):
     book_file_contents = book_file.read()
   
   return book_file_contents
-
-# def count_words(text):
-#   return len(text.split())
-
-# def count_chars(text):
-#   char_dict = {}
-
-#   text = re.sub('[^a-zA-Z]', '', text)
-#   text = text.lower()
-
-#   # print(text)
-
-#   for char in text:
-#     if char_dict.get(char):
-#       char_dict[char] += 1
-#     else:
-#       char_dict[char] = 1
-  
-#   return char_dict
 
 main()
 
