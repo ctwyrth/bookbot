52da9994800f5a36c5fe0299c20d6a4e3e3ae019
Thomas W. <96929397+ctwyrth@users.noreply.github.com>
2025-07-06 10:30:08 -0700

    trying to fix the dict

--- stats.py
+++ stats.py
@@ -7,15 +7,16 @@ def count_words(text):
 def count_chars(text):
   char_dict = {}
 
-  text = re.sub('[^a-zA-Z]', '', text)
-  text = text.lower()
+  # text = re.sub('[^a-zA-Z]', '', text)
+  # text = text.lower()
 
   # print(text)
 
   for char in text:
-    if char_dict.get(char):
-      char_dict[char] += 1
+    lower_char = char.lower()
+    if lower_char in char_dict:
+      char_dict[lower_char] += 1
     else:
-      char_dict[char] = 1
+      char_dict[lower_char] = 1
   
   return char_dict
